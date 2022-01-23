5193308c5036df98fb463244991495a9e141d51f
jubenjam <49247806+jubenjam@users.noreply.github.com>
2022-01-23 14:47:05 -0800

    Player doesn't slide down stairs

--- Senior Project/Assets/Scripts/Player/PlayerMovement.cs
+++ Senior Project/Assets/Scripts/Player/PlayerMovement.cs
@@ -9,16 +9,23 @@ public class PlayerMovement : MonoBehaviour
     float horizontalMovement = 0f;
     bool jump = false;
     bool crouch = false;
+    bool moveInput = false;
 
     // Update is called once per frame
     void Update()
     {
+        moveInput = false;
         //get horizontal movement
         horizontalMovement = Input.GetAxisRaw("Horizontal") * runSpeed;
+        if(horizontalMovement != 0)
+        {
+            moveInput = true;
+        }
         //check for jumping
         if (Input.GetButtonDown("Jump"))
         {
             jump = true;
+            moveInput = true;
         }
         //check for crouching    
         if (Input.GetButtonDown("Crouch"))
@@ -35,8 +42,28 @@ public class PlayerMovement : MonoBehaviour
     // FixedUpdate is called a fixed amount of times per second
     void FixedUpdate()
     {
+
         //Move character (fixedDeltaTime makes sure movement is at same speed on different computers)
         controller.Move(horizontalMovement * Time.fixedDeltaTime, crouch, jump);
         jump = false; //don't jump forever
+    }
+
+    void OnCollisionStay2D(Collision2D collision){
+        if(collision.gameObject.tag == "Stairs")
+        {
+            if(moveInput == true){
+                gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+            }
+            else{
+                gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
+            }
+        }
+    }    
+
+    void OnCollisionExit2D(Collision2D collision){
+        if(collision.gameObject.tag == "Stairs")
+        {
+            gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+        }
     }
 }
